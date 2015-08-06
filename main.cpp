1f203c878f3ee537186d52cd103c69bae9b876c1
Thomas Fischer <thomas.fischer@his.se>
2015-08-06 12:34:19 +0200

    Making use of IdTree's and SwedishTreeTree's ability to be dumped and read from iostreams

--- main.cpp
+++ main.cpp
@@ -9,51 +9,92 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const char *inputfilename = (argc < 2) ? "sweden.osm.pbf" : argv[argc - 1];
-    ifstream fp(inputfilename, ifstream::in | ifstream::binary);
+#ifdef DEBUG
+    Error::debug("DEBUG flag enabled");
+#endif // DEBUG
+
+    char filenamebuffer[1024];
+    const char *country = (argc < 2) ? "sweden" : argv[argc - 1];
+    snprintf(filenamebuffer, 1024, "/home/fish/git/pbflookup/%s.osm.pbf", country);
+    ifstream fp(filenamebuffer, ifstream::in | ifstream::binary);
     if (fp) {
+        SwedishText::Tree *swedishTextTree = NULL;
+        IdTree<Coord> *n2c = NULL;
+        IdTree<WayNodes> *w2n = NULL;
+        IdTree<RelationMem> *relmem = NULL;
 
-        OsmPbfReader osmPbfReader;
-        SwedishText::Tree *swedishTextTree;
-        IdTree<Coord> *n2c;
-        IdTree<WayNodes> *w2n;
-        IdTree<RelationMem> *relmem;
-        const bool result = osmPbfReader.parse(fp, &swedishTextTree, &n2c, &w2n, &relmem);
+        snprintf(filenamebuffer, 1024, "/tmp/%s.texttree", country);
+        ifstream fileteststream(filenamebuffer);
+        if (fileteststream && fileteststream.good()) {
+            fileteststream.seekg(0, fileteststream.end);
+            const int length = fileteststream.tellg();
+            fileteststream.seekg(0, fileteststream.beg);
 
+            if (length < 10) {
+                OsmPbfReader osmPbfReader;
+                osmPbfReader.parse(fp, &swedishTextTree, &n2c, &w2n, &relmem);
+            }
+        } else {
+            OsmPbfReader osmPbfReader;
+            osmPbfReader.parse(fp, &swedishTextTree, &n2c, &w2n, &relmem);
+        }
+        fileteststream.close();
         /// Clean up the protobuf lib
         google::protobuf::ShutdownProtobufLibrary();
-
         fp.close();
 
-        /*
-        while (1) {
-            if (!std::cin.good()) {
-                std::cout << std::endl;
-                break;
-            }
-            std::cout << std::endl << "New query: ";
-            std::string keyboardinput;
-            std::getline(std::cin, keyboardinput);
-            const char *query = keyboardinput.c_str();
-            if (query[0] == 0 || query[0] < 0x20) {
-                std::cout << std::endl;
-                break;
-            }
-
-            std::vector< uint64_t> ids = swedishTextTree->retrieve_ids(query);
-            for (unsigned int i = 0; i < ids.size(); ++i) {
-                std::cout << "  http://www.openstreetmap.org/";
-                switch (ids[i] & 3) {
-                case NODE_NIBBLE: std::cout << "node/"; break;
-                case WAY_NIBBLE: std::cout << "way/"; break;
-                case RELATION_NIBBLE: std::cout << "relation/"; break;
-                default: break;
-                }
-                std::cout << (ids[i] >> 2) << std::endl;
-            }
-            std::cout << "Number of hits: " << ids.size() << std::endl;
+        if (swedishTextTree != NULL) {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.texttree", country);
+            Error::debug("Writing %s", filenamebuffer);
+            ofstream swedishtexttreefile(filenamebuffer);
+            swedishTextTree->write(swedishtexttreefile);
+            swedishtexttreefile.close();
+        } else {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.texttree", country);
+            Error::debug("Reading %s", filenamebuffer);
+            ifstream swedishtexttreefile(filenamebuffer);
+            swedishTextTree = new SwedishText::Tree(swedishtexttreefile);
+            swedishtexttreefile.close();
         }
-        */
+        if (n2c != NULL) {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.n2c", country);
+            Error::debug("Writing %s", filenamebuffer);
+            ofstream n2cfile(filenamebuffer);
+            n2c->write(n2cfile);
+            n2cfile.close();
+        } else {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.n2c", country);
+            Error::debug("Reading %s", filenamebuffer);
+            ifstream n2cfile(filenamebuffer);
+            n2c = new IdTree<Coord>(n2cfile);
+            n2cfile.close();
+        }
+        if (w2n != NULL) {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.w2n", country);
+            Error::debug("Writing %s", filenamebuffer);
+            ofstream w2nfile(filenamebuffer);
+            w2n->write(w2nfile);
+            w2nfile.close();
+        } else {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.w2n", country);
+            Error::debug("Reading %s", filenamebuffer);
+            ifstream w2nfile(filenamebuffer);
+            w2n = new IdTree<WayNodes>(w2nfile);
+            w2nfile.close();
+        }
+        if (relmem != NULL) {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.relmem", country);
+            Error::debug("Writing %s", filenamebuffer);
+            ofstream relmemfile(filenamebuffer);
+            relmem->write(relmemfile);
+            relmemfile.close();
+        } else {
+            snprintf(filenamebuffer, 1024, "/tmp/%s.relmem", country);
+            Error::debug("Reading %s", filenamebuffer);
+            ifstream relmemfile(filenamebuffer);
+            relmem = new IdTree<RelationMem>(relmemfile);
+            relmemfile.close();
+        }
 
         std::ifstream textfile("input.txt");
         if (textfile.is_open()) {
@@ -62,37 +103,39 @@ int main(int argc, char *argv[])
             tokenizer.read_words(textfile, words);
             textfile.close();
 
-            static const size_t combined_len = 8188;
-            char combined[combined_len + 4];
-            for (int s = 3; s >= 1; --s) {
-                for (int i = 0; i <= words.size() - s; ++i) {
-                    char *p = combined;
-                    for (int k = 0; k < s; ++k) {
-                        if (k > 0)
-                            p += snprintf(p, combined_len - (p - combined), " ");
-                        p += snprintf(p, combined_len - (p - combined), "%s", words[i + k].c_str());
-                    }
+            if (relmem != NULL && w2n != NULL && n2c != NULL && swedishTextTree != NULL) {
+                static const size_t combined_len = 8188;
+                char combined[combined_len + 4];
+                for (int s = 3; s >= 1; --s) {
+                    for (int i = 0; i <= words.size() - s; ++i) {
+                        char *p = combined;
+                        for (int k = 0; k < s; ++k) {
+                            if (k > 0)
+                                p += snprintf(p, combined_len - (p - combined), " ");
+                            p += snprintf(p, combined_len - (p - combined), "%s", words[i + k].c_str());
+                        }
 
-                    std::vector<uint64_t> id_list = swedishTextTree->retrieve_ids(combined);
-                    if (!id_list.empty()) {
-                        Error::debug("Got %i hits for word '%s' (s=%i)", id_list.size(), combined, s);
-                        for (int l = 0; l < id_list.size(); ++l) {
-                            const uint64_t id = id_list[l] >> 2;
-                            Error::debug("  id=%llu", id);
-                            const int lowerBits = id_list[l] & 3;
-                            if (lowerBits == NODE_NIBBLE) {
-                                Coord c;
-                                const bool found = n2c->retrieve(id, c);
-                                if (found)
-                                    Error::debug("       lat=%lf lon=%lf (found=%i)", c.lat, c.lon);
+                        std::vector<uint64_t> id_list = swedishTextTree->retrieve_ids(combined);
+                        if (!id_list.empty()) {
+                            Error::debug("Got %i hits for word '%s' (s=%i)", id_list.size(), combined, s);
+                            for (int l = 0; l < id_list.size(); ++l) {
+                                const uint64_t id = id_list[l] >> 2;
+                                Error::debug("  id=%llu", id);
+                                const int lowerBits = id_list[l] & 3;
+                                if (lowerBits == NODE_NIBBLE) {
+                                    Coord c;
+                                    const bool found = n2c->retrieve(id, c);
+                                    if (found)
+                                        Error::debug("       lat=%lf lon=%lf (found=%i)", c.lat, c.lon);
+                                    else
+                                        Error::debug("    is unknown Node");
+                                } else if (lowerBits == WAY_NIBBLE)
+                                    Error::debug("    is Way");
+                                else if (lowerBits == RELATION_NIBBLE)
+                                    Error::debug("    is Relation");
                                 else
-                                    Error::debug("    is unknown Node");
-                            } else if (lowerBits == WAY_NIBBLE)
-                                Error::debug("    is Way");
-                            else if (lowerBits == RELATION_NIBBLE)
-                                Error::debug("    is Relation");
-                            else
-                                Error::info("  Neither node, way, nor relation");
+                                    Error::info("  Neither node, way, nor relation");
+                            }
                         }
                     }
                 }
