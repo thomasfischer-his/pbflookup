5a7f1ebff617665bd9f1153027f17aeca541c240
Thomas Fischer <thomas.fischer@his.se>
2015-08-18 21:47:26 +0200

    Adding license header to source files (.h and .cpp)

--- osmpbfreader.h
+++ osmpbfreader.h
@@ -1,3 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2015 by Thomas Fischer <thomas.fischer@his.se>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 3 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This code is heavily based on file  tools/osmpbf-outline.cpp          *
+ *   from OSM-binary:  https://github.com/scrosby/OSM-binary               *
+ *   OSM-binary is also licensed under the GNU Public License v3.          *
+ *                                                                         *
+ *   Authors of   tools/osmpbf-outline.cpp   include (in alphabetic        *
+ *   order):                                                               *
+ *     alex85k <alexei_kasatkin@mail.ru>                                   *
+ *     Bas Couwenberg <sebastic@xs4all.nl>                                 *
+ *     Charlie Root <root@freebsd.vvtis>                                   *
+ *     Dmitry Marakasov <amdmi3@amdmi3.ru>                                 *
+ *     Frederik Ramm <frederik@remote.org>                                 *
+ *     Hartmut Holzgraefe <hartmut@php.net>                                *
+ *     Jochen Topf <jochen@topf.org>                                       *
+ *     kayrus <kay.diam@gmail.com>                                         *
+ *     keine-ahnung <keine.ahnung.ka@googlemail.com>                       *
+ *     michael <michael@192.168.1.3>                                       *
+ *     Peter <github@mazdermind.de>                                        *
+ *     Scott Crosby <scrosby@cs.rice.edu>                                  *
+ *     scrosby <scrosby@cs.rice.edu>                                       *
+ *     Thomas Friebel <yaron@codefreax.com>                                *
+ ***************************************************************************/
+
 #ifndef OSMPBFREADER_H
 #define OSMPBFREADER_H
 
