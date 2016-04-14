7d673251d211fade296542cd7a6e8cebfb348586
Thomas Fischer <thomas.fischer@his.se>
2016-04-14 22:33:46 +0200

    Greatly improving handling of UTF-8 character

--- helper.cpp
+++ helper.cpp
@@ -99,7 +99,7 @@ bool getCenterOfOSMElement(const OSMElement &element, Coord &coord) {
         return false;
 }
 
-void handleCombiningDiacriticalMark(std::string &text, size_t i) {
+bool handleCombiningDiacriticalMark(std::string &text, size_t i) {
     /// Assumption: text[i] == 0xcc
     const unsigned char textPlusOne = (unsigned char)(text[i + 1]);
     const unsigned char textMinusOne = (unsigned char)(text[i - 1]);
@@ -109,43 +109,364 @@ void handleCombiningDiacriticalMark(std::string &text, size_t i) {
             if (textMinusOne == 'e') {
                 text[i - 1] = 0xc3; text[i] = 0xa9;
                 text.erase(i + 1, 1);
+                return true;
             } else if (textMinusOne == 'E') {
                 text[i - 1] = 0xc3; text[i] = 0x89;
                 text.erase(i + 1, 1);
+                return true;
             }
         } else if (textPlusOne == 0x88) { /// Combining Diaeresis
             if (textMinusOne == 'a') {
                 text[i - 1] = 0xc3; text[i] = 0xa4;
                 text.erase(i + 1, 1);
+                return true;
             } else if (textMinusOne == 'A') {
                 text[i - 1] = 0xc3; text[i] = 0x84;
                 text.erase(i + 1, 1);
+                return true;
             } else if (textMinusOne == 'o') {
                 text[i - 1] = 0xc3; text[i] = 0xb6;
                 text.erase(i + 1, 1);
+                return true;
             } else if (textMinusOne == 'o') {
                 text[i - 1] = 0xc3; text[i] = 0x96;
                 text.erase(i + 1, 1);
+                return true;
             }
         } else if (textPlusOne == 0x8a) { /// Combining Ring Above
             if (textMinusOne == 'a') {
                 text[i - 1] = 0xc3; text[i] = 0xa5;
                 text.erase(i + 1, 1);
+                return true;
             } else if (textMinusOne == 'A') {
                 text[i - 1] = 0xc3; text[i] = 0x85;
                 text.erase(i + 1, 1);
+                return true;
             }
         }
     }
+
+    return false;
 }
 
-void correctutf8mistakes(std::string &text, size_t i) {
+bool extendedLatinToAscii(std::string &text, size_t i) {
     const unsigned char c = text[i];
     const unsigned char next_c = i < text.length() - 1 ? text[i + 1] : 0;
+    if (c == 0xc3) {
+        if (next_c >= 0x80 && next_c <= 0x83) {
+            text[i] = 'A';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0x86) {
+            text[i] = 'A';
+            text[i + 1] = 'E';
+            return true;
+        } else if (next_c == 0x87) {
+            text[i] = 'C';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x88 && next_c <= 0x8b) {
+            text[i] = 'E';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x8c && next_c <= 0x8f) {
+            text[i] = 'I';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0x90) {
+            text[i] = 'D';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0x91) {
+            text[i] = 'N';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (((next_c >= 0x92 && next_c <= 0x95) || next_c == 0x98)) {
+            text[i] = 'O';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x99 && next_c <= 0x9c) {
+            text[i] = 'I';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0x9d) {
+            text[i] = 'Y';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0x9f) {
+            text[i] = 's';
+            text[i + 1] = 's';
+            return true;
+        } else if (next_c >= 0xa0 && next_c <= 0xa3) {
+            text[i] = 'a';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0xa6) {
+            text[i] = 'a';
+            text[i + 1] = 'e';
+            return true;
+        } else if (next_c == 0xa7) {
+            text[i] = 'c';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xa8 && next_c <= 0xab) {
+            text[i] = 'e';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xac && next_c <= 0xaf) {
+            text[i] = 'i';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0xb0) {
+            text[i] = 'd';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0xb1) {
+            text[i] = 'n';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (((next_c >= 0xb2 && next_c <= 0xb5) || next_c == 0xb8)) {
+            text[i] = 'o';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xb9 && next_c <= 0xbc) {
+            text[i] = 'i';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0xbd) {
+            text[i] = 'y';
+            text.erase(i + 1, 1);
+            return true;
+        }
+    } else if (c == 0xc4) {
+        if (next_c >= 0x80 && next_c <= 0x85) {
+            if ((next_c & 1) == 0) ///< upper-case 'A'
+                text[i] = 'A';
+            else ///< lower-case 'a'
+                text[i] = 'a';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x86 && next_c <= 0x8d) {
+            if ((next_c & 1) == 0) ///< upper-case 'C'
+                text[i] = 'C';
+            else ///< lower-case 'c'
+                text[i] = 'c';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x8e && next_c <= 0x91) {
+            if ((next_c & 1) == 0) ///< upper-case 'D'
+                text[i] = 'D';
+            else ///< lower-case 'd'
+                text[i] = 'd';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x92 && next_c <= 0x9b) {
+            if ((next_c & 1) == 0) ///< upper-case 'E'
+                text[i] = 'E';
+            else ///< lower-case 'e'
+                text[i] = 'e';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x9c && next_c <= 0xa3) {
+            if ((next_c & 1) == 0) ///< upper-case 'G'
+                text[i] = 'G';
+            else ///< lower-case 'g'
+                text[i] = 'g';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xa4 && next_c <= 0xa7) {
+            if ((next_c & 1) == 0) ///< upper-case 'H'
+                text[i] = 'H';
+            else ///< lower-case 'h'
+                text[i] = 'h';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xa8 && next_c <= 0xb1) {
+            if ((next_c & 1) == 0) ///< upper-case 'H'
+                text[i] = 'H';
+            else ///< lower-case 'h'
+                text[i] = 'h';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c == 0xb2) {
+            text[i] = 'I';
+            text[i + 1] = 'J';
+            return true;
+        } else if (next_c == 0xb3) {
+            text[i] = 'i';
+            text[i + 1] = 'j';
+            return true;
+        } else if (next_c >= 0xb4 && next_c <= 0xb5) {
+            if ((next_c & 1) == 0) ///< upper-case 'J'
+                text[i] = 'J';
+            else ///< lower-case 'j'
+                text[i] = 'j';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0xb9 && next_c <= 0xbe) {
+            if ((next_c & 1) == 1) ///< upper-case 'L'
+                text[i] = 'L';
+            else ///< lower-case 'l'
+                text[i] = 'l';
+            text.erase(i + 1, 1);
+            return true;
+        }
+    } else if (c == 0xc5) {
+        if (next_c >= 0x83 && next_c <= 0x88) {
+            if ((next_c & 1) == 1) ///< upper-case 'N'
+                text[i] = 'N';
+            else ///< lower-case 'n'
+                text[i] = 'n';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x8a && next_c <= 0x8b) {
+            if ((next_c & 1) == 0) ///< upper-case 'N'
+                text[i] = 'N';
+            else ///< lower-case 'n'
+                text[i] = 'n';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x8c && next_c <= 0x91) {
+            if ((next_c & 1) == 0) ///< upper-case 'O'
+                text[i] = 'O';
+            else ///< lower-case 'o'
+                text[i] = 'o';
+            text.erase(i + 1, 1);
+            return true;
+        } else if (next_c >= 0x9a && next_c <= 0xa1) {
+            if ((next_c & 1) == 0) ///< upper-case 'S'
+                text[i] = 'S';
+            else ///< lower-case 's'
+                text[i] = 's';
+            text.erase(i + 1, 1);
+            return true;
+        }
+    } else {
+        const unsigned char next2_c = i < text.length() - 2 ? text[i + 2] : 0;
+
+        if (c == 0xe1) {
+            if (next_c == 0xba && next2_c >= 0xa0 && next2_c <= 0xb7) {
+                if ((next2_c & 1) == 0) ///< upper-case 'A'
+                    text[i] = 'A';
+                else ///< lower-case 'a'
+                    text[i] = 'a';
+                text.erase(i + 1, 2);
+                return true;
+            } else if (((next_c == 0xba && next2_c >= 0xb8) || (next_c == 0xbb && next2_c <= 0x87))) {
+                if ((next2_c & 1) == 0) ///< upper-case 'E'
+                    text[i] = 'E';
+                else ///< lower-case 'e'
+                    text[i] = 'e';
+                text.erase(i + 1, 2);
+                return true;
+            } else if (next_c == 0xbb && next2_c >= 0x88 && next2_c <= 0x8b) {
+                if ((next2_c & 1) == 0) ///< upper-case 'I'
+                    text[i] = 'I';
+                else ///< lower-case 'i'
+                    text[i] = 'i';
+                text.erase(i + 1, 2);
+                return true;
+            } else if (next_c == 0xbb && next2_c >= 0x8c && next2_c <= 0xa3) {
+                if ((next2_c & 1) == 0) ///< upper-case 'O'
+                    text[i] = 'O';
+                else ///< lower-case 'o'
+                    text[i] = 'o';
+                text.erase(i + 1, 2);
+                return true;
+            } else if (next_c == 0xbb && next2_c >= 0xa4 && next2_c <= 0xb1) {
+                if ((next2_c & 1) == 0) ///< upper-case 'U'
+                    text[i] = 'U';
+                else ///< lower-case 'u'
+                    text[i] = 'u';
+                text.erase(i + 1, 2);
+                return true;
+            } else if (next_c == 0xbb && next2_c >= 0xb2 && next2_c <= 0xb9) {
+                if ((next2_c & 1) == 0) ///< upper-case 'Y'
+                    text[i] = 'Y';
+                else ///< lower-case 'y'
+                    text[i] = 'y';
+                text.erase(i + 1, 2);
+                return true;
+            }
+        }
+
+        // TODO
+    }
+    // TODO
+
+    return false;
+}
+
+
+bool symbolsToAscii(std::string &text, size_t i) {
+    const unsigned char c = text[i];
+    const unsigned char next_c = i < text.length() - 1 ? text[i + 1] : 0;
+
+    if (c == 0xe2 && next_c == 0x80) {
+        const unsigned char next2_c = i < text.length() - 2 ? text[i + 2] : 0;
+        if (next2_c >= 0x92 && next2_c <= 0x95) {
+            /// Some form of a dash
+            text[i] = '-';
+            text.erase(i + 1, 2);
+            return true;
+        }
+        // TODO
+    } else if (c == 0xe2 && next_c == 0x86) {
+        const unsigned char next2_c = i < text.length() - 2 ? text[i + 2] : 0;
+        if (next2_c >= 0x90 || next2_c >= 0x92 || next2_c >= 0x94 || next2_c >= 0xbc || next2_c >= 0xbd) {
+            /// Some form of an arrow
+            text[i] = '-';
+            text.erase(i + 1, 2);
+            return true;
+        }
+        // TODO
+    } else if (c == 0xe2 && next_c == 0x87) {
+        const unsigned char next2_c = i < text.length() - 2 ? text[i + 2] : 0;
+        if (next2_c >= 0x80 || next2_c >= 0x81 || next2_c >= 0x90 || next2_c >= 0x92) {
+            /// Some form of an arrow
+            text[i] = '-';
+            text.erase(i + 1, 2);
+            return true;
+        }
+        // TODO
+    }
+    // TODO
+
+    return false;
+}
+
+bool correctutf8mistakes(std::string &text, size_t &i) {
+    const unsigned char c = text[i];
+    const unsigned char next_c = i < text.length() - 1 ? text[i + 1] : 0;
+    const unsigned char next2_c = i < text.length() - 2 ? text[i + 2] : 0;
+
     if (c == 0xc2 && next_c == 0xb4) { /// Acute Accent (spacing character)
         text[i] = '\'';
         text.erase(i + 1, 1);
+        return true;
+    } else if (c == 0xc2 && next_c == 0xae) { /// Registered Trademark, ...
+        text.erase(i, 2);
+        --i;
+        return true;
+    } else if (c == 0xe2 && next_c == 0x84 && next2_c == 0xa2) { /// Registered Trademark, ...
+        text.erase(i, 3);
+        --i;
+        return true;
+    } else if (c == 0xe2 && next_c == 0x80) {
+        if ((next2_c >= 0x9c && next2_c <= 0x9f) || next2_c == 0xb3 || next2_c == 0xb6) {
+            /// Something like a double quotation mark
+            text[i] = '"';
+            text.erase(i + 1, 2);
+            return true;
+        } else if ((next2_c >= 0x98 && next2_c <= 0x9b) || next2_c == 0xb2 || next2_c == 0xb5) {
+            /// Something like a single quotation mark
+            text[i] = '\'';
+            text.erase(i + 1, 2);
+            return true;
+        }
     }
+
+    return false;
 }
 
 unsigned char utf8tolower(const unsigned char &prev_c, unsigned char c) {
@@ -176,10 +497,17 @@ std::string &utf8tolower(std::string &text) {
         const unsigned char c = text[i];
         if (i > 0 && c == 0xcc) {
             handleCombiningDiacriticalMark(text, i);
+            prev_c = text[i] = utf8tolower(prev_c, text[i]);
+        } else if (c == 0xc2 || c == 0xe2) {
+            /// Observation: mapper use by mistake UTF-8 sequences starting with 0xc2 or 0xe2
+            bool madeChanges = correctutf8mistakes(text, i);
+            if (!madeChanges)
+                madeChanges = symbolsToAscii(text, i);
             prev_c = text[i];
-        } else if (c == 0xc2 /** observation: mapper use by mistake UTF-8 sequences starting with 0xc2 */) {
-            correctutf8mistakes(text, i);
-            prev_c = text[i];
+        } else if ((c >= 0xc3 && c <= 0xc5) || c == 0xe1) {
+            /// Rewrite extended Latin characters into their plain ASCII counterparts
+            extendedLatinToAscii(text, i);
+            prev_c = text[i] = utf8tolower(prev_c, text[i]);
         } else
             prev_c = text[i] = utf8tolower(prev_c, c);
     }
