8eeceb81da318dfa6e3ff9c3dfda7b75290c6978
Thomas Fischer <thomas.fischer@his.se>
2016-04-28 15:52:42 +0200

    Adapting copyright year to 2015-2016

--- idtree_impl.h
+++ idtree_impl.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2015 by Thomas Fischer <thomas.fischer@his.se>          *
+ *   Copyright (C) 2015-2016 by Thomas Fischer <thomas.fischer@his.se>     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
