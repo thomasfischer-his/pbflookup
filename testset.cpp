db097a04efd1e204729a3ed019c4edec135aa984
Thomas Fischer <thomas.fischer@his.se>
2016-05-14 23:08:15 +0200

    Show debug output only if compiler flag DEBUG is set

--- testset.cpp
+++ testset.cpp
@@ -49,7 +49,8 @@ void Testset::run() {
                 return a.quality > b.quality;
             });
 
-            Error::info("Found %d many possible results for testset '%s':", results.size(), it->name.c_str());
+            Error::info("Found %d many possible results for testset '%s'", results.size(), it->name.c_str());
+#ifdef DEBUG
             for (const Result &result : results) {
                 const double lon = Coord::toLongitude(result.coord.x);
                 const double lat = Coord::toLatitude(result.coord.y);
@@ -67,6 +68,7 @@ void Testset::run() {
                         Error::debug("  http://www.openstreetmap.org/?mlat=%.5f&mlon=%.5f#map=12/%.5f/%.5f", lat, lon, lat, lon);
                     }
             }
+#endif // DEBUG
         } else
             Error::warn("Unable to determine a likely position");
 
